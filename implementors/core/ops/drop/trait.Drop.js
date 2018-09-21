2295700728c069cae763116813509a44ebf45bbf
Travis CI User <travis@example.org>
2018-09-21 20:27:15 +0000

    Documentation for alexcrichton/tokio-process@1638c32

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tokio_process"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"tokio_process/struct.Child.html\" title=\"struct tokio_process::Child\">Child</a>",synthetic:false,types:["tokio_process::Child"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
